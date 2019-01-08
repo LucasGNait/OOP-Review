090f69d7434946c4f51285fbe5809c9ca13713e5
LucasGNait <46492942+LucasGNait@users.noreply.github.com>
2019-01-08 09:22:32 -0700

    Only Stub

--- src/CSharp.Language.Review/Program.cs
+++ src/CSharp.Language.Review/Program.cs
@@ -8,9 +8,12 @@ namespace CSharp.Language.Review
 {
     class Program
     {
+        private static Random rnd = new Random();
         static void Main(string[] args)
         {
+            Program app = new Program(args);
 
+            app.AssignMarks(30, 80);
 
         }
     }
