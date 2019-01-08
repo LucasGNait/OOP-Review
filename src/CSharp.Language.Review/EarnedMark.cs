be432347a8f79f6c988ff3e1fae5681e7a3e8607
LucasGNait <46492942+LucasGNait@users.noreply.github.com>
2019-01-08 09:35:43 -0700

    mid way through coding

--- src/CSharp.Language.Review/EarnedMark.cs
+++ src/CSharp.Language.Review/EarnedMark.cs
@@ -8,5 +8,24 @@ namespace CSharp.Language.Review
 {
     class EarnedMark
     {
+        public int Possible { get; private set; }
+        private double _Earned;
+        public double Earned
+        {
+            get { return _Earned; }
+            set
+            {
+                if (value < 0 || value > Possible)
+                    throw new Exception("Invalid earned mark assigned");
+                _Earned = value;
+            }
+        }
+        public double Percent
+        { get { return (Earned / Possible) * 100; } }
+
+        public double WeightedPercent
+        { get { return Percent * WeightedPercent / 100; } }
+
+        //public EarnedMark(WeightedMark markableItem, int possible, double earned) : this(markableItem.Name, markableItem.Weight, possible, earned) { }
     }
 }
