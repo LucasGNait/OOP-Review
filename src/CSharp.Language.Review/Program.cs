a3c655164d8624e02b00ac2074bb0822645cc14b
LucasGNait <46492942+LucasGNait@users.noreply.github.com>
2019-01-11 11:25:39 -0700

    Added comments to Program.cs

--- src/CSharp.Language.Review/Program.cs
+++ src/CSharp.Language.Review/Program.cs
@@ -1,16 +1,30 @@
-﻿using System;
+﻿// Identifies the namespaces containing the data type(s)
+// that we want to use (or reference) in the code in this
+// file.
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+// Declares an "area" or named-space in which we can
+// place our programmer-defined data types.
 namespace CSharp.Language.Review
 {
-    class Program
+    // The namespace plus the class name is what's called
+    // a "fully-qualified" class name.
+    // The fully-qualified class name for program is
+    //   CSharp.Language.Review.Program
+    public class Program
     {
+        // A static field initialized to a new Random object
         private static Random rnd = new Random();
+
+        // Main() is the entry point
         static void Main(string[] args)
         {
+            // The body of the Main() method
+            // acts as the "driver" of my application
             Program app = new Program(args);
 
             app.AssignMarks(30, 80);
@@ -26,14 +40,22 @@ namespace CSharp.Language.Review
 
         }
 
+        // This field is acting as a "backing store"
+        // for the Students property.
         private List<Student> _students = new List<Student>();
 
+        // This property provides "controlled access"
+        // to the data in the backing store (the field).
         public List<Student> Students
         {
             get { return _students; }
             set { _students = value; }
         }
 
+        // This is a constructor.
+        // The job of a constructor is to ensure
+        // that all of the fields/properties
+        // have "meaningful" values.
         public Program(string[] studentNames)
         {
             WeightedMark[] CourseMarks = new WeightedMark[4];
@@ -53,6 +75,12 @@ namespace CSharp.Language.Review
                
             }
         }
+
+        /// <summary>
+        /// This assignes a random mark to each student in the <see cref="Students"/> property.
+        /// </summary>
+        /// <param name="min">The minimum possible earned value for the student's mark</param>
+        /// <param name="max">The maximum possible earned value for the student's mark</param>
 
         public void AssignMarks(int min, int max)
         {
